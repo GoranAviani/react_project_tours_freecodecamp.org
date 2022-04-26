19102c9ca61e6ba1edf589d67581963336882111
GoranAviani <goranaviani@gmail.com>
2022-04-26 19:29:26 +0200

    react_project_tours_freecodecamp.org

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [tours, setTours] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [tours, setTours] = useState([])
 
-   const fetchTours = async () => {
-      setLoading(true)
-       try{
-        const response = await fetch(url)
-       const tours = await response.json()
-        console.log(tours)
-         setLoading(false)
+    const fetchTours = async () => {
+        setLoading(true)
+        try {
+            const response = await fetch(url)
+            const tours = await response.json()
+            console.log(tours)
+            setLoading(false)
 
-       }catch(error){
-setLoading(false)
-           console.log(error)
-       }
-
-
-   }
-
-   useEffect(()=>{fetchTours()}, [])
+        } catch (error) {
+            setLoading(false)
+            console.log(error)
+        }
 
 
-  if (loading){
+    }
+
+    useEffect(() => {
+        fetchTours()
+    }, [])
+
+
+    if (loading) {
+        return (
+            <div>
+                <Loading/>
+            </div>
+        )
+    }
     return (
-        <div>
-          <Loading/>
-        </div>
+        <main>
+            <Tours/>
+        </main>
     )
-  }
-  return (
-      <main>
-        <Tours/>
-      </main>
-  )
 
 }
 
